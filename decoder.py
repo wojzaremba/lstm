e2563ed1c480753198d8f05ada15bd9fecd887e4
Lang-Chi Yu <yl871804@gmail.com>
2015-10-15 02:28:45 +0800

    decoder

--- decoder.py
+++ decoder.py
@@ -1,13 +1,21 @@
 from sys import argv
 
 
-if len(argv) != 4:
-    print "Usage: python decoder.py <input> <output> <dict>"
+if len(argv) != 4 and len(argv) != 5:
+    print "Usage: python decoder.py <input> <output> <dict> [output_true y|N]"
     exit(1)
 
 input_ = argv[1]
 output = argv[2]
 dictionary = argv[3]
+if len(argv) == 5:
+    output_true = argv[4]
+    if output_true == 'y':
+        output_true = True
+    else:
+        output_true = False
+else:
+    output_true = False
 
 d_arr = ['0']
 with open(dictionary, 'r') as d:
@@ -16,8 +24,10 @@ with open(dictionary, 'r') as d:
         d_arr.append(line)
 
 with open(input_, 'r') as fin, \
-    open(output+".true", 'w') as fout_true, \
     open(output, 'w') as fout:
+
+    if output_true:
+        fout_true = open(output+".true", 'w')
 
     for line in fin:
         eos = False
@@ -27,11 +37,16 @@ with open(input_, 'r') as fin, \
             if word == '<eos>':
                 eos = True
 
-            fout_true.write(word)
-            fout_true.write(' ')
+            if output_true:
+                fout_true.write(word)
+                fout_true.write(' ')
             if not eos:
                 fout.write(word)
                 fout.write(' ')
 
-        fout_true.write('\n')
+        if output_true:
+            fout_true.write('\n')
         fout.write('\n')
+
+    if output_true:
+        fout_true.close()
